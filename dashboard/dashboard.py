a07385e93a1af5cc8fb297fc4478740d77fcad27
Muhammad Farhan Al Rahman <maretaaliana03@gmail.com>
2024-03-04 15:27:41 +0700

    commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,199 +4,196 @@ import matplotlib.image as mpimg
 import seaborn as sns
 import streamlit as st
 import urllib
-from func import DataAnalyzer, BrazilMapPlotter
+from function import DataAnalyzer, BrazilMapPlotter
 
-sns.set(style='dark')
+sns.set(style='whitegrid')  # Ganti gaya grafik menjadi whitegrid
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
-datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/df.csv")
-all_df.sort_values(by="order_approved_at", inplace=True)
-all_df.reset_index(inplace=True)
+date_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+ecommerce_data = pd.read_csv("https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/df.csv")
+ecommerce_data.sort_values(by="order_approved_at", inplace=True)
+ecommerce_data.reset_index(drop=True, inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/geolocation.csv')
-data = geolocation.drop_duplicates(subset='customer_unique_id')
+geo_data = pd.read_csv('https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/geolocation.csv')
+unique_customers_data = geo_data.drop_duplicates(subset='customer_unique_id')
 
-for col in datetime_cols:
-    all_df[col] = pd.to_datetime(all_df[col])
+for col in date_cols:
+    ecommerce_data[col] = pd.to_datetime(ecommerce_data[col])
 
-min_date = all_df["order_approved_at"].min()
-max_date = all_df["order_approved_at"].max()
+min_date_val = ecommerce_data["order_approved_at"].min()
+max_date_val = ecommerce_data["order_approved_at"].max()
 
 # Sidebar
 with st.sidebar:
-    col1, col2, col3 = st.columns(3)
-    with col1:
+    col_left, col_center, col_right = st.columns(3)
+    with col_left:
         st.write(' ')
-    with col2:
-        st.image("https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/logo1.png"
-                 , width=100)
-    with col3:
+    with col_center:
+        st.image("https://raw.githubusercontent.com/Maretaaliana/data-analyst-dicoding/main/dashboard/logo2.png", width=100)  # Ganti logo
+    with col_right:
         st.write(' ')
 
-    # Date Range
-    start_date, end_date = st.date_input(
-        label="Select Date Range",
-        value=[min_date, max_date],
-        min_value=min_date,
-        max_value=max_date
+    # Rentang Tanggal
+    start_date_val, end_date_val = st.date_input(
+        label="Pilih Rentang Tanggal",
+        value=[min_date_val, max_date_val],
+        min_value=min_date_val,
+        max_value=max_date_val
     )
 
 # Main
-main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
-                 (all_df["order_approved_at"] <= str(end_date))]
+main_data = ecommerce_data[(ecommerce_data["order_approved_at"] >= str(start_date_val)) & 
+                           (ecommerce_data["order_approved_at"] <= str(end_date_val))]
 
-function = DataAnalyzer(main_df)
-map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
+data_analyzer = DataAnalyzer(main_data)
+brazil_map_plotter = BrazilMapPlotter(unique_customers_data, plt, mpimg, urllib, st)
 
-daily_orders_df = function.create_daily_orders_df()
-sum_spend_df = function.create_sum_spend_df()
-sum_order_items_df = function.create_sum_order_items_df()
-review_score, common_score = function.review_score_df()
-state, most_common_state = function.create_bystate_df()
-order_status, common_status = function.create_order_status()
+daily_orders_df = data_analyzer.create_daily_orders_df()
+total_spend_df = data_analyzer.create_sum_spend_df()
+total_order_items_df = data_analyzer.create_sum_order_items_df()
+review_scores, common_score = data_analyzer.review_score_df()
+customer_state, most_common_state = data_analyzer.create_bystate_df()
+order_statuses, common_status = data_analyzer.create_order_status()
 
-# Define your Streamlit app
-st.title("E-Commerce Public Data Analysis")
+# Tentukan aplikasi Streamlit Anda
+st.title("Analisis Data Publik E-Commerce")
 
-# Add text or descriptions
-st.write("**This is a dashboard for analyzing E-Commerce public data.**")
+# Tambahkan teks atau deskripsi
+st.write("**Ini adalah dasbor untuk menganalisis data publik E-Commerce.**")
 
-# Daily Orders Delivered
-st.subheader("Daily Orders Delivered")
+# Pesanan Harian yang Dikirim
+st.subheader("Pesanan Harian yang Dikirim")
 col1, col2 = st.columns(2)
 
 with col1:
-    total_order = daily_orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
+    total_order_val = daily_orders_df["order_count"].sum()
+    st.markdown(f"Total Pesanan: **{total_order_val}**")
 
 with col2:
-    total_revenue = daily_orders_df["revenue"].sum()
-    st.markdown(f"Total Revenue: **{total_revenue}**")
+    total_revenue_val = daily_orders_df["revenue"].sum()
+    st.markdown(f"Total Pendapatan: **{total_revenue_val}**")
 
-fig, ax = plt.subplots(figsize=(12, 6))
+fig_orders, ax_orders = plt.subplots(figsize=(12, 6))
 sns.lineplot(
     x=daily_orders_df["order_approved_at"],
     y=daily_orders_df["order_count"],
     marker="o",
     linewidth=2,
-    color="#90CAF9"
+    color="#FFA07A"  # Ganti warna garis
 )
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
+ax_orders.tick_params(axis="x", rotation=45)
+ax_orders.tick_params(axis="y", labelsize=15)
+st.pyplot(fig_orders)
 
-# Customer Spend Money
-st.subheader("Customer Spend Money")
+# Pengeluaran Uang Pelanggan
+st.subheader("Pengeluaran Uang Pelanggan")
 col1, col2 = st.columns(2)
 
 with col1:
-    total_spend = sum_spend_df["total_spend"].sum()
-    st.markdown(f"Total Spend: **{total_spend}**")
+    total_spend_val = total_spend_df["total_spend"].sum()
+    st.markdown(f"Total Pengeluaran: **{total_spend_val}**")
 
 with col2:
-    avg_spend = sum_spend_df["total_spend"].mean()
-    st.markdown(f"Average Spend: **{avg_spend}**")
+    avg_spend_val = total_spend_df["total_spend"].mean()
+    st.markdown(f"Rata-rata Pengeluaran: **{avg_spend_val}**")
 
-fig, ax = plt.subplots(figsize=(12, 6))
+fig_spend, ax_spend = plt.subplots(figsize=(12, 6))
 sns.lineplot(
-    data=sum_spend_df,
+    data=total_spend_df,
     x="order_approved_at",
     y="total_spend",
     marker="o",
     linewidth=2,
-    color="#90CAF9"
+    color="#FFA07A"  # Ganti warna garis
 )
 
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
-st.pyplot(fig)
+ax_spend.tick_params(axis="x", rotation=45)
+ax_spend.tick_params(axis="y", labelsize=15)
+st.pyplot(fig_spend)
 
-# Order Items
-st.subheader("Order Items")
+# Barang Pesanan
+st.subheader("Barang Pesanan")
 col1, col2 = st.columns(2)
 
 with col1:
-    total_items = sum_order_items_df["product_count"].sum()
-    st.markdown(f"Total Items: **{total_items}**")
+    total_items_val = total_order_items_df["product_count"].sum()
+    st.markdown(f"Total Barang: **{total_items_val}**")
 
 with col2:
-    avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
+    avg_items_val = total_order_items_df["product_count"].mean()
+    st.markdown(f"Rata-rata Barang: **{avg_items_val}**")
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+fig_items, ax_items = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
 
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="viridis", ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=80)
-ax[0].set_title("Most sold products", loc="center", fontsize=90)
-ax[0].tick_params(axis ='y', labelsize=55)
-ax[0].tick_params(axis ='x', labelsize=50)
+sns.barplot(x="product_count", y="product_category_name_english", data=total_order_items_df.head(5), palette="mako", ax=ax_items[0])  # Ganti palette
+ax_items[0].set_ylabel(None)
+ax_items[0].set_xlabel("Jumlah Penjualan", fontsize=80)
+ax_items[0].set_title("Produk paling laris", loc="center", fontsize=90)
+ax_items[0].tick_params(axis ='y', labelsize=55)
+ax_items[0].tick_params(axis ='x', labelsize=50)
 
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="viridis", ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=80)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Fewest products sold", loc="center", fontsize=90)
-ax[1].tick_params(axis='y', labelsize=55)
-ax[1].tick_params(axis='x', labelsize=50)
+sns.barplot(x="product_count", y="product_category_name_english", data=total_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="mako", ax=ax_items[1])  # Ganti palette
+ax_items[1].set_ylabel(None)
+ax_items[1].set_xlabel("Jumlah Penjualan", fontsize=80)
+ax_items[1].invert_xaxis()
+ax_items[1].yaxis.set_label_position("right")
+ax_items[1].yaxis.tick_right()
+ax_items[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=90)
+ax_items[1].tick_params(axis='y', labelsize=55)
+ax_items[1].tick_params(axis='x', labelsize=50)
 
-st.pyplot(fig)
+st.pyplot(fig_items)
 
-# Review Score
-st.subheader("Review Score")
+# Skor Ulasan
+st.subheader("Skor Ulasan")
 col1, col2 = st.columns(2)
 
 with col1:
-    avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score:.2f}**")
+    avg_review_score_val = review_scores.mean()
+    st.markdown(f"Rata-rata Skor Ulasan: **{avg_review_score_val:.2f}**")
 
 with col2:
-    most_common_review_score = review_score.value_counts().idxmax()
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
+    most_common_review_score_val = review_scores.value_counts().idxmax()
+    st.markdown(f"Skor Ulasan Paling Umum: **{most_common_review_score_val}**")
 
-fig, ax = plt.subplots(figsize=(12, 6))
-colors = sns.color_palette("viridis", len(review_score))
+fig_reviews, ax_reviews = plt.subplots(figsize=(12, 6))
+colors_reviews = sns.color_palette("mako", len(review_scores))
 
-sns.barplot(x=review_score.index,
-            y=review_score.values,
-            order=review_score.index,
-            palette=colors)
+sns.barplot(x=review_scores.index,
+            y=review_scores.values,
+            order=review_scores.index,
+            palette=colors_reviews)
 
-plt.title("Customer Review Scores for Service", fontsize=15)
+plt.title("Skor Ulasan Pelanggan untuk Layanan", fontsize=15)
 plt.xlabel("Rating")
-plt.ylabel("Count")
+plt.ylabel("Jumlah")
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 
-# Menambahkan label di atas setiap bar
-for i, v in enumerate(review_score.values):
-    ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
+for i, v in enumerate(review_scores.values):
+    ax_reviews.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
 
-st.pyplot(fig)
+st.pyplot(fig_reviews)
 
-# Customer Demographic
-st.subheader("Customer Demographic")
+# Demografi Pelanggan
+st.subheader("Demografi Pelanggan")
 
-most_common_state = state.customer_state.value_counts().index[0]
-st.markdown(f"Most Common State: **{most_common_state}**")
+most_common_state_val = customer_state.customer_state.value_counts().index[0]
+st.markdown(f"Negara Bagian Paling Umum: **{most_common_state_val}**")
 
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.barplot(x=state.customer_state.value_counts().index,
-            y=state.customer_count.values, 
-            data=state,
-            palette="viridis"
+fig_state, ax_state = plt.subplots(figsize=(12, 6))
+sns.barplot(x=customer_state.customer_state.value_counts().index,
+            y=customer_state.customer_count.values, 
+            data=customer_state,
+            palette="mako"
             )
 
-plt.title("Number customers from State", fontsize=15)
-plt.xlabel("State")
-plt.ylabel("Number of Customers")
+plt.title("Jumlah pelanggan dari Setiap Negara Bagian", fontsize=15)
+plt.xlabel("Negara Bagian")
+plt.ylabel("Jumlah Pelanggan")
 plt.xticks(fontsize=12)
-st.pyplot(fig)
-
+st.pyplot(fig_state)
 
 st.caption('Mareta Aliana')
