9e475bc2db56a7ca79cbb4eca16b3685ea8ee9ea
Muhammad Farhan Al Rahman <maretaaliana03@gmail.com>
2024-03-04 15:41:53 +0700

    -

--- dashboard/function.py
+++ dashboard/function.py
@@ -6,19 +6,31 @@ class DataAnalyzer:
         daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
             "order_id": "nunique",
             "payment_value": "sum"
-        }).reset_index().rename(columns={"order_id": "order_count", "payment_value": "revenue"})
+        })
+        daily_orders_df = daily_orders_df.reset_index()
+        daily_orders_df.rename(columns={
+            "order_id": "order_count",
+            "payment_value": "revenue"
+        }, inplace=True)
         
         return daily_orders_df
     
     def create_sum_spend_df(self):
         sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
             "payment_value": "sum"
-        }).reset_index().rename(columns={"payment_value": "total_spend"})
+        })
+        sum_spend_df = sum_spend_df.reset_index()
+        sum_spend_df.rename(columns={
+            "payment_value": "total_spend"
+        }, inplace=True)
 
         return sum_spend_df
 
     def create_sum_order_items_df(self):
-        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index().rename(columns={"product_id": "product_count"})
+        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+        sum_order_items_df.rename(columns={
+            "product_id": "product_count"
+        }, inplace=True)
         sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
 
         return sum_order_items_df
@@ -30,7 +42,10 @@ class DataAnalyzer:
         return review_scores, most_common_score
 
     def create_bystate_df(self):
-        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index().rename(columns={"customer_id": "customer_count"})
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
         most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
         bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
 
